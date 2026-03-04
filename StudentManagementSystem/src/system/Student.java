a6c3c9d5f0f810f510bbc5c8b9fc0f56ec714f86
Mina Nosshy <mnosshy7@gmail.com>
2026-03-04 20:44:00 +0200

    Add files via upload

---
+++ StudentManagementSystem/src/system/Student.java
@@ -0,0 +1,21 @@
+package system;
+
+public class Student {
+
+    String name;
+    int id;
+    double grade;
+
+    public Student(String name, int id, double grade) {
+        this.name = name;
+        this.id = id;
+        this.grade = grade;
+    }
+
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Grade: " + grade);
+        System.out.println("-------------------");
+    }
+}
